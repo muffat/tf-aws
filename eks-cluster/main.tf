d85e07824da32fe81d2830adbdb66e59d97446c8
ulhaqf <fathi.ulhaq@autodesk.com>
2021-07-19 17:02:57 +0800

    Update

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -10,25 +10,6 @@ resource "aws_eks_cluster" "main" {
   }
 }
 
-resource "aws_iam_role" "example" {
-  name = "eks-cluster-example"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
 output "endpoint" {
   value = aws_eks_cluster.main.endpoint
 }
