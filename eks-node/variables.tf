c573702abb2439057ec02cdb8e3db3fa6a10bf93
ulhaqf <fathi.ulhaq@autodesk.com>
2021-07-19 17:50:43 +0800

    Update

--- eks-node/variables.tf
+++ eks-node/variables.tf
@@ -3,7 +3,9 @@ variable "node_group_name" {}
 variable "node_role_arn" {}
 variable "subnet_ids" {}
 variable "instance_types" {}
-variable "labels" {}
+variable "labels" {
+  default = ""
+}
 variable "ec2_ssh_key" {}
 variable "source_security_group_ids" {}
 variable "desired_size" {}
